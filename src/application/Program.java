13d0dda4c25bca7e381a91cd27094f3e4592a8be
Elyson2k <elysonvinicius@outlook.com>
2022-09-22 10:22:26 -0300

    Criando classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department dp = new Department(1,"A");
 		System.out.println(dp);
+		
+		Seller sl = new Seller(1,"Elyson", "Elyson@Gmail.com", new Date(), 3000.0, dp);
+		System.out.println(sl);
 
 	}
 
