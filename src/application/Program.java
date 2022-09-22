95fb0647aac899784cc4c50cc65f68ad6ae5e83b
Elyson2k <elysonvinicius@outlook.com>
2022-09-22 10:12:21 -0300

    Criando classe Department

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(1,"A");
+		System.out.println(dp);
+
+	}
+
+}
